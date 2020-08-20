732468996a4fde1a6cf64f5cbba74d2952c67263
Sanjana <sanjhemdev@gmail.com>
2020-08-21 04:06:19 +0530

    Add color methods to the String class

--- main.rb
+++ main.rb
@@ -1,3 +1,21 @@
-class Color
+class String
+  # Adding colors
+  def use_code(color_code)
+    "\e[#{color_code}m#{self}\e[0m"
+  end
+
+  def red;          use_code(31) end
+  def green;        use_code(32) end
+  def yellow;       use_code(33) end
+  def blue;         use_code(34) end
+  def magenta;      use_code(35) end
+  def cyan;         use_code(36) end
+
+  def bg_red;       use_code(41) end
+  def bg_green;     use_code(42) end
+  def bg_yellow;    use_code(43) end
+  def bg_blue;      use_code(44) end
+  def bg_magenta;   use_code(45) end
+  def bg_cyan;      use_code(46) end
 
 end
