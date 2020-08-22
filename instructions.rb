49bd80dcf2f356d32a97c9424bbf7c462489335e
Sanjana <sanjhemdev@gmail.com>
2020-08-22 15:37:20 +0530

    Fix spelling error

--- instructions.rb
+++ instructions.rb
@@ -21,6 +21,6 @@ module Instructions
   end
 
   def valid_code
-    "A valid code is one which contains 4 letters with no spaces between them. Each letter represents one color, as follows:" + "\n-- red, " + 'r'.red + "\n-- Green, " + 'g'.green + "\n-- Yellow, " + 'y'.yellow + "\n-- Blue, " + 'b'.blue + "\n-- Magenta, " + 'm'.magenta + "\n-- Teal, " + 't'.teal
+    "\nA valid code is one which contains 4 lowercase letters with no spaces between them. Each letter represents one color, as follows:" + "\n-- red, " + 'r'.red + "\n-- Green, " + 'g'.green + "\n-- Yellow, " + 'y'.yellow + "\n-- Blue, " + 'b'.blue + "\n-- Magenta, " + 'm'.magenta + "\n-- Teal, " + 't'.teal
   end
 end
